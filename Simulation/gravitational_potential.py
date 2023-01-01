44f12449858368f2d78192d89f43bad8f2d43997
(Jack) Kuei Hung Zhang <kh4zhang@uwaterloo.ca>
2023-01-01 16:58:18 -0500

    Fixed gravitational potential generator
    The original script used for calculating the gravitational potential at
    each point on a set grid was intended for a specific ratio of the two large
    masses. The script has been fixed so that it works properly with other
    ratios. Since it is expected that the gravity_potential function will
    receive a wide range of values for its parameters, it will now throw
    exceptions for parameters that don't make physical sense being passed.

--- Simulation/gravitational_potential.py
+++ Simulation/gravitational_potential.py
@@ -1,4 +1,4 @@
-from logging import DEBUG, getLogger, info, StreamHandler
+from logging import DEBUG, getLogger, StreamHandler
 from matplotlib.pyplot import figure, contour, colorbar, show
 from numba import jit
 from numpy import meshgrid, linspace, sqrt, array
@@ -10,57 +10,72 @@ from time import process_time, perf_counter
 def gravity_potential(m1: float, m2: float, m2_orbital_radius: float,
                       x_points: array, y_points: array) -> array:
     """
-    Creates a contour plot of the gravitational potential for the smaller third mass. The proportion of M1 that is
-    equivalent to the mass M2 is used because it better captures the behaviour of the system based on the relative
-    sizes of the two large bodies.
+    Takes an array of coordinates from a numpy.meshgrid object and determines the ratio of the gravitational potential
+    and the mass of the small object at each point in the meshgrid. The ratio is used rather than the potential field
+    itself because the resulting field will be something that holds true for any small third object in the three body
+    system, rather generating a similarly shaped field but with varying values proportional to the mass of the third
+    object.
 
     :param m1: Mass of the largest object in the simulated 3 body system
-    :param m2: Decimal ratio of the mass M2 to M1
+    :param m2: Mass of the second-largest object in the simulated 3 body system
     :param m2_orbital_radius: orbital radius of the object of mass M2
     :param x_points: x-coordinates of the grid
     :param y_points: y-coordinates of the grid
+
     :return: None
+
+    :raise ValueError: Either of the masses is non-positive or m2 is larger than m1
     """
 
+    if m1 <= 0:
+        raise ValueError(f"The value of m1 must be positive, received {m1}")
+
+    if m2 <= 0:
+        raise ValueError(f"The value of m2 must be positive, received {m2}")
+
+    if m2 > m1:
+        raise ValueError(f" The value of m1 must be positive but less than or equal to m2.\n"
+                         f" I.e. the ratio of m2 to m1 must be in (0, 1], received {m2/m1}")
+
     gravitational_constant = 1
-    m2_absolute = m2 * m1
 
-    omega = sqrt(3 * gravitational_constant * m2_absolute / m2_orbital_radius)
+    # By convention,  omega is used as the variable representing angular frequency
+    omega = sqrt(gravitational_constant * (m1 + m2) / m2_orbital_radius**3)
 
-    # Note that since M1/(M1+M2) gets reduced to 2/3 and M2/(M1+M2) gets reduced to 1/3
-    # Due to our earlier definitions for M1 and M2                                                            #
-    # Also note that Z as defined above will result in an indentation error,
-    # but was typed this way, so it could fit on one page
-    gravitational_potential = -0.5 * (omega ** 2) * (x_points ** 2 + y_points ** 2)
-    -(2 * gravitational_constant * m2_absolute) / (sqrt((m2_orbital_radius / 3 - x_points) ** 2 + y_points ** 2))
-    -(gravitational_constant * m2_absolute) / (sqrt((m2_orbital_radius * 2 / 3 - x_points) ** 2 + y_points ** 2))
+    gravitational_potential = -0.5 * (omega ** 2) * (x_points ** 2 + y_points ** 2) - gravitational_constant * (
+            m2 / (sqrt((m2 * m2_orbital_radius / (m1 + m2) - x_points)**2 + y_points**2))
+            + m2 / (sqrt((m1 * m2_orbital_radius / (m1 + m2) + x_points)**2 + y_points**2)))
 
     return gravitational_potential
 
 
-if __name__ == "__main__":
+def main() -> None:
     # Function calls for plot_gravity_potential
-    M1 = 2
-    M2 = 0.5
-    R = 1
+    large_mass = 2
+    second_mass = 0.5
+    orbital_radius = 1
 
     # Defining the x,y coordinates of our plot
     plot_length = 200
     grid_x, grid_y = meshgrid(linspace(-1.5, 1.5, plot_length), linspace(-1.5, 1.5, plot_length))
 
-    # Configure logger
-    log = getLogger()
+    # Configure logger, with the scope name used as the logger name
+    log = getLogger(__name__)
     log.setLevel(DEBUG)
     log.addHandler(StreamHandler(stdout))
 
     start_elapsed = perf_counter()
     start_process = process_time()
-    grid_z = gravity_potential(M1, M2, R, grid_x, grid_y)
+    grid_z = gravity_potential(large_mass, second_mass, orbital_radius, grid_x, grid_y)
     figure(figsize=(10, 10), dpi=100)
     contour(grid_x, grid_y, grid_z, 500)
     colorbar()
     show()
     end_elapsed = perf_counter()
     end_process = process_time()
-    info(f"Elapsed time: {end_elapsed - start_elapsed}")
-    info(f"Process time: {end_process - start_process}")
+    log.info(f"Elapsed time: {end_elapsed - start_elapsed}")
+    log.info(f"Process time: {end_process - start_process}")
+
+
+if __name__ == "__main__":
+    main()
